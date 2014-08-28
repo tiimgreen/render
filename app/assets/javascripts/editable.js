63c374da17af7a7e769c08b1ae0c14f9a60efccb
Tim Green <tiimgreen@gmail.com>
2014-08-28 21:48:25 +0100

    Add bootstrap

--- app/assets/javascripts/editable.js
+++ app/assets/javascripts/editable.js
@@ -1,4 +1,12 @@
+function createEditableModal() {
+
+}
+
 $('.editable').each(function(index) {
   // Skip this element if a modal has already been attached
-  if ($(this).attr('data-modal-attached') == 1) continue;
+  if ($(this).attr('data-modal-attached') == 1) {
+    return;
+  }
+
+  createEditableModal();
 });
